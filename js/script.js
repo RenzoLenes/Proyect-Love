4e4c3a73f09466fce3b3c490b21aa4cee8907efc
Not2023 <jt1496788@gmail.com>
2023-11-03 13:04:53 -0500

    video added

--- js/script.js
+++ js/script.js
@@ -7,3 +7,30 @@ function moveButton() {
 
 // Llama a la función moveButton cuando el mouse pasa por encima del botón "No"
 noBtn.addEventListener("mouseover", moveButton);
+
+var siBtn = document.getElementById("siBtn");
+var videoPopup = document.getElementById("videoPopup");
+var overlay = document.getElementById("overlay");
+var closeButton = document.getElementById("closeButton");
+
+// Función para mostrar el video emergente y el fondo oscuro
+function showVideo() {
+    overlay.style.display = "block"; // Muestra el fondo oscuro
+    videoPopup.style.display = "block"; // Muestra el video emergente
+    videoPopup.play(); // Inicia la reproducción del video
+}
+
+// Función para ocultar el video emergente y el fondo oscuro
+function hideVideo() {
+    overlay.style.display = "none"; // Oculta el fondo oscuro
+    videoPopup.style.display = "none"; // Oculta el video emergente
+    videoPopup.pause(); // Pausa la reproducción del video
+}
+
+// Asigna la función showVideo al evento click del botón "Sí"
+siBtn.addEventListener("click", showVideo);
+
+// Asigna la función hideVideo al evento click del video emergente (para cerrarlo)
+videoPopup.addEventListener("click", hideVideo);
+
+closeButton.addEventListener("click", hideVideo);
